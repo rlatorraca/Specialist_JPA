0b989b2acc0250f6e3947135ffedea2e0fcf1f42
Rodrigo Latorraca <rlatorraca@gmail.com>
2020-03-07 13:11:25 -0400

    Buscar objetos por ID (2.6)

--- specialist-jpa-ecommerce/src/test/java/com/rlsp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
+++ specialist-jpa-ecommerce/src/test/java/com/rlsp/ecommerce/iniciandocomjpa/ConsultandoRegistrosTest.java
@@ -8,6 +8,11 @@ import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.rlsp.ecommerce.model.Produto;
+
+import org.junit.Assert;;
 
 public class ConsultandoRegistrosTest {
 
@@ -46,4 +51,38 @@ public class ConsultandoRegistrosTest {
 		entityManager.close();
 	}
 	
+	/**
+	 * BUSCA atraves do identificador (ID)
+	 */
+	
+	@Test
+	public void buscaPorId() {
+		
+		//Passa a CLASSE + ID
+		// find() ==> busca as informacoes no DB, diretamente. Nao espera ser invocado alguma propriedade
+		Produto produto = entityManager.find(Produto.class, 1);
+		
+		// getReference () ==> enquanto nao se usa um propriedade (getNome(); getPreco(); etc)  de PRODUTO o Hibernate nao busca nada no DB
+		Produto produto2 = entityManager.getReference(Produto.class, 1);
+		
+		Assert.assertNotNull(produto);
+		Assert.assertEquals("Kindle", produto.getNome());
+		
+		System.out.println(produto.getPreco());
+		
+	}
+	
+	@Test
+	public void atualizarReferencia() {
+		
+		Produto produto = entityManager.find(Produto.class, 1);
+		produto.setNome("Laptop ASUS"); // troca o valor no DB
+		System.out.println(produto.getNome());
+		
+		
+		
+		entityManager.refresh(produto); // Faz o REFRESh (Reiniciar) tabela  PRODUTO para os estado ANTERIOR com 'Kindle'
+		System.out.println(produto.getNome());
+		Assert.assertEquals("Kindle", produto.getNome());
+	}
 }
